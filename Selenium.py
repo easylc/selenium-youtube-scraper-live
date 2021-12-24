d26a95d068d1b29824721e7bbed89d2495ea2d4f
easylc <33836769+easylc@users.noreply.github.com>
2021-12-24 05:51:13 +0000

    Selenium - find element

--- Selenium.py
+++ Selenium.py
@@ -12,6 +12,12 @@ def  get_driver():
   driver = webdriver.Chrome(options=chrome_options)
   return driver
 
+def get_videos(driver):
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  driver.get(YOUTUBE_TRENDING_URL)
+  videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+  return videos
+
 if __name__ == "__main__":
   print('Fetching the page')
   driver = get_driver()
@@ -22,5 +28,28 @@ if __name__ == "__main__":
   TAG ='ytd-video-name-render' 
   video_divs = driver.find_elements(By.TAG_NAME, TAG)
   
-  print(f'Found {len(video_divs)} videos')
+  videos = get_videos(driver)
+
+  print(f'Found {len(videos)} videos')
   print('Page Title',driver.title)
+  
+  
+  #print (video)
+  #title, url, thumbnail, channel, views, uploaded, description
+
+  video = videos[0]
+  title_tag = video.find_element(By.ID, 'video-title')
+  
+  title = title_tag.text
+  url = title_tag.get_attribute('href')
+  thumbnail_tag = video.find_element(By.TAG_NAME, 'img')
+  thumbnail_url = thumbnail_tag.get_attribute('src')
+
+  channel_div = video.find_element(By.CLASS_NAME,  'ytd-channel-name')
+
+  channel_name = channel_div.text
+  
+  print('Video Title:',title)
+  print('Video URL:',url)
+  print('Video Thumbnail URL:', thumbnail_url)
+  print('Video Channel Name:', channel_name)
