a406fd763f0528c68075101935c25bd0f90cc230
easylc <33836769+easylc@users.noreply.github.com>
2022-01-02 16:31:54 +0000

    Property => get details with dynamic fields

--- property.py
+++ property.py
@@ -14,7 +14,7 @@ def  get_driver():
   return driver
 
 class trxnClass:
-  def __init__(self, url, postdate, upddate, usagetype, district, addreng, addrchi, bldeng, bldchi, unit, price, indate, landyr):
+  def __init__(self, url, postdate, upddate, usagetype, district, addreng, addrchi, bldeng, bldchi, price, remark, indate, landyr):
     self.url = url
     self.postdate = postdate
     self.upddate = upddate
@@ -24,8 +24,8 @@ class trxnClass:
     self.addrchi = addrchi
     self.bldeng = bldeng
     self.bldchi = bldchi
-    self.unit = unit
     self.price = price
+    self.remark = remark
     self.indate = indate
     self.landyr = landyr
 
@@ -53,10 +53,47 @@ if __name__ == "__main__":
   results = result_all.find_elements(By.XPATH, "//span[@class='saleprice']")
   #print('Details:',len(results))
   
-  links = result_all.find_elements(By.XPATH, "//*[text()='樓盤詳情']").get_attribute('href')
+  links = result_all.find_elements(By.XPATH, "//*[text()='樓盤詳情']")
   
+  driver1 = get_driver()
+
   for each_link in links:
-    driver.get(each_link)
-    propDiv = driver.find_element(By.ID,"property-info")
-    val = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[1]/td[@class='val']")
-    print("val",val) 
+    link = each_link.get_attribute('href')
+    driver1.get(link)
+    print("link:",link) 
+    propDiv = driver1.find_element(By.ID,"property-info")
+    postdate = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[1]/td[@class='val']").get_attribute('innerHTML')
+    print("postdate:",postdate) 
+
+    upddate = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[2]/td[@class='val']").get_attribute('innerHTML')
+    print("upddate:",upddate) 
+
+    usagetype = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[3]/td[@class='val']").get_attribute('innerHTML')
+    print("usagetype:",usagetype) 
+    
+    district = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[4]/td[@class='val']").get_attribute('innerHTML')
+    print("district:",district) 
+
+    addreng = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[5]/td[@class='val']").get_attribute('innerHTML')
+    print("addreng:",addreng) 
+
+    addrchi = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[6]/td[@class='val']").get_attribute('innerHTML')
+    print("addrchi:",addrchi) 
+    
+    bldeng = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[7]/td[@class='val']").get_attribute('innerHTML')
+    print("bldeng:",bldeng) 
+
+    bldchi = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[8]/td[@class='val']").get_attribute('innerHTML')
+    print("bldchi:",bldchi) 
+
+    price = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[9]/td[@class='val']").get_attribute('innerHTML')
+    print("price:",price) 
+    
+    remark = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[10]/td[@class='val']").get_attribute('innerHTML')
+    print("remark:",remark) 
+
+    indate = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[11]/td[@class='val']").get_attribute('innerHTML')
+    print("indate:",indate) 
+
+    landyr = propDiv.find_element(By.XPATH, "//div[2]/table[1]/tbody[1]/tr[12]/td[@class='val']").get_attribute('innerHTML')
+    print("landyr:",landyr) 
