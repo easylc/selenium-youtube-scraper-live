67f7759f702e337144a11d1a365076fd07b8de42
easylc <33836769+easylc@users.noreply.github.com>
2022-01-05 05:54:48 +0000

    Property HK with details

--- property.py
+++ property.py
@@ -7,6 +7,8 @@ import re
 
 PROPERTY_TRENDING_URL = 'https://property.hk/property_search.php?bldg='+urllib.parse.quote("永利中心", safe='')+'&prop=P&pt=A&loc=&dt=&saleType=1&greenform=&fh=&parking='
 
+PROPERTY_TXN_TRENDING_URL = 'https://property.hk/tran.php?dt=&bldg='+urllib.parse.quote("永利中心", safe='')+'&year&prop=P&saleType=3&loc='
+
 def  get_driver():
   chrome_options = Options()
   chrome_options.add_argument('--no-sandbox')
@@ -15,12 +17,22 @@ def  get_driver():
   driver = webdriver.Chrome(options=chrome_options)
   return driver
 
-class trxnClass:
+class propSalesClass:
   def __init__(self, url, postdate, upddate, floor, price):
     self.url = url
     self.postdate = postdate
     self.upddate = upddate
     self.floor = floor
+    self.price = price
+
+
+class propTxnClass:
+  def __init__(self, url, postdate, ref_num, docdate, addr, price):
+    self.url = url
+    self.postdate = postdate
+    self.ref_num = ref_num
+    self.docdate = docdate
+    self.addr = addr
     self.price = price
 
 
@@ -51,6 +63,8 @@ if __name__ == "__main__":
   
   driver1 = get_driver()
 
+  print ("For Sales")
+  
   for each_link in links:
     link = each_link.get_attribute('href')
     driver1.get(link)
@@ -76,3 +90,45 @@ if __name__ == "__main__":
     price = re.split('<span', price,1)
     print("售價(萬):",price[0]) 
     
+print ("Transaction History")
+driver2 = get_driver()
+driver2.get(PROPERTY_TXN_TRENDING_URL)
+propTxnDiv = driver2.find_element(By.ID,"proplist")
+
+TxnLinks = propTxnDiv.find_elements(By.XPATH, "//*[text()='詳情']")
+
+driver3 = get_driver()
+
+for each_txn_link in TxnLinks:
+  txn_link = each_txn_link.get_attribute('href')
+  #print("Txn Link:",txn_link) 
+
+  driver3.get(txn_link)
+  propTxnDetailDiv = driver3.find_element(By.CLASS_NAME,"col-xs-12")
+  postdateTxt = propTxnDetailDiv.find_element(By.XPATH, "//*[text()='登記日期']")
+  postdate = postdateTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"col-xs-9").get_attribute('innerHTML')
+  print("登記日期:",postdate) 
+
+  refNumTxt = propTxnDetailDiv.find_element(By.XPATH, "//*[text()='登記編號']")
+  refNum = refNumTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"col-xs-9").get_attribute('innerHTML')
+  print("登記編號:",refNum) 
+
+  docdateTxt = propTxnDetailDiv.find_element(By.XPATH, "//*[text()='文件日期']")
+  docdate = docdateTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"col-xs-9").get_attribute('innerHTML')
+  print("文件日期:",docdate) 
+
+  addrTxt = propTxnDetailDiv.find_element(By.XPATH, "//*[text()='地址']")
+  addr = docdateTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"col-xs-9").get_attribute('innerHTML')
+  print("地址:",addr)
+
+  priceTxt = propTxnDetailDiv.find_element(By.XPATH, "//*[text()='售價']")
+  price = priceTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"col-xs-9").get_attribute('innerHTML')
+  price = re.split('<a', price,1)
+  price = re.split(' ', price[0],1)
+  print("售價（百萬）:",price[0])
+
+  
+
+
+
+
