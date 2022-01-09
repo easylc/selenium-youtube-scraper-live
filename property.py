d290a703c436b4c7deca0028e91ad0ce7a2235e6
easylc <33836769+easylc@users.noreply.github.com>
2022-01-09 11:07:23 +0000

    Export to CSV

--- property.py
+++ property.py
@@ -4,11 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import urllib.parse
 import re
-
+import pandas as pd
 
 PROPERTY_TRENDING_URL = 'https://property.hk/property_search.php?bldg='+urllib.parse.quote("永利中心", safe='')+'&prop=P&pt=A&loc=&dt=&saleType=1&greenform=&fh=&parking='
 
 PROPERTY_TXN_TRENDING_URL = 'https://property.hk/tran.php?dt=&bldg='+urllib.parse.quote("永利中心", safe='')+'&year&prop=P&saleType=3&loc='
+
+sales_data_list = []
+trxns_data_list = []
 
 def  get_driver():
   chrome_options = Options()
@@ -27,26 +30,6 @@ def  get_mb_driver():
   driver = webdriver.Chrome(options=chrome_options)
   return driver
 
-
-class propSalesClass:
-  def __init__(self, url, postdate, upddate, floor, price):
-    self.url = url
-    self.postdate = postdate
-    self.upddate = upddate
-    self.floor = floor
-    self.price = price
-
-
-class propTxnClass:
-  def __init__(self, url, postdate, ref_num, docdate, addr, price):
-    self.url = url
-    self.postdate = postdate
-    self.ref_num = ref_num
-    self.docdate = docdate
-    self.addr = addr
-    self.price = price
-
-
 if __name__ == "__main__":
   print('Fetching the page - Property')
   driver = get_driver()
@@ -55,25 +38,20 @@ if __name__ == "__main__":
   stypeElement.click()
 
   searchDiv = driver.find_element(By.CLASS_NAME,"searchqueryitem")
-  #print('Search:',searchDiv.get_attribute('innerHTML'))
   resultDiv = driver.find_element(By.CLASS_NAME,"searchresult")
-  #print('Result:',resultDiv.get_attribute('innerHTML'))
-
   result_all = driver.find_element(By.NAME,"printForm")
-  #print('Result:',result_all.get_attribute('innerHTML'))
-  results = result_all.find_elements(By.XPATH, "//span[@class='saleprice']")
-  #print('Details:',len(results))
-  
+  results = result_all.find_elements(By.XPATH, "//span[@class='saleprice']") 
   links = result_all.find_elements(By.XPATH, "//*[text()='樓盤詳情']")
   
   driver1 = get_driver()
 
-  print ("For Sales")
+  print ("== Part(1)  Print Sales ==")
+  
   i = 1
   for each_link in links:
     link = each_link.get_attribute('href')
     driver1.get(link)
-    #print("link:",link) 
+    
     propDiv = driver1.find_element(By.ID,"property-info")
     postdateTxt = propDiv.find_element(By.XPATH, "//*[text()='更新日期']")
     postdate = postdateTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"val").get_attribute('innerHTML')
@@ -88,18 +66,22 @@ if __name__ == "__main__":
       floor = floorTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"val").get_attribute('innerHTML')
       print(i,".層數及單位:",floor)
     except NoSuchElementException:
+      floor = 'NA'
       print(i,".層數及單位:Not Found")
 
     priceTxt = propDiv.find_element(By.XPATH, "//*[text()='售價 ']")
     price = priceTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"val").get_attribute('innerHTML')
     price = re.split('<span', price,1) 
     price = price[0][1:]
-    price = int(price)*10000 
-    print(i,".售價(萬):","${:,.2f}".format(price))
+    price = int(price)*10000
+    price = "${:,.2f}".format(price)
+    print(i,".售價(萬):",price)
+    
+    sales_data_list.append(['永利中心','P',link,postdate,upddate,floor,price])
     i=i+1
 
 
-print ("For Transaction History")
+print ("== Part(2)  Print Transaction History(20) ==")
 j=1
 driver2 = get_driver()
 driver2.get(PROPERTY_TXN_TRENDING_URL)
@@ -111,8 +93,7 @@ driver3 = get_driver()
 
 for each_txn_link in TxnLinks:
   txn_link = each_txn_link.get_attribute('href')
-  #print("Txn Link:",txn_link) 
-
+  
   driver3.get(txn_link)
   propTxnDetailDiv = driver3.find_element(By.CLASS_NAME,"col-xs-12")
   postdateTxt = propTxnDetailDiv.find_element(By.XPATH, "//*[text()='登記日期']")
@@ -128,20 +109,27 @@ for each_txn_link in TxnLinks:
   print(j,".文件日期:",docdate) 
 
   addrTxt = propTxnDetailDiv.find_element(By.XPATH, "//*[text()='地址']")
-  addr = docdateTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"col-xs-9").get_attribute('innerHTML')
+  addr = addrTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"col-xs-9").get_attribute('innerHTML')
   print(j,".地址:",addr)
 
   priceTxt = propTxnDetailDiv.find_element(By.XPATH, "//*[text()='售價']")
   price = priceTxt.find_element(By.XPATH, "..").find_element(By.CLASS_NAME,"col-xs-9").get_attribute('innerHTML')
   price = re.split('<a', price,1)
   price = re.split(' ', price[0],1)
-  price = float(price[0])*1000000 
-  print(j,".售價（萬）:","${:,.2f}".format(price))
-
+  price = float(price[0])*1000000
+  price = "${:,.2f}".format(price)
+  print(j,".售價（萬）:",price)
+  
+  trxns_data_list.append(['永利中心','P',link,postdate,refNum,docdate,addr,price])
   j=j+1
 
-  
+print ("== Part(3)  Export CSV ==")
+sales_df = pd.DataFrame(sales_data_list,columns=['keyword','type','url','postdate','upddate','floor','price'],index=None)
+print(sales_df)
+sales_df.to_csv('sales_trending.csv')
 
+trxns_df = pd.DataFrame(trxns_data_list,columns=['keyword','type','url','postdate','refNum','docdate','address','price'],index=None)
+print(trxns_df)
+trxns_df.to_csv('trxns_trending.csv')
 
-
-
+print ("== End ==")
